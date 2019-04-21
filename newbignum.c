ff6fa1f8e32d91f28cb74feba4f57fab4aad5dc1
Victor Machado <machado.prx@gmail.com>
2019-04-21 03:17:53 -0300

    big is easy

--- newbignum.c
+++ newbignum.c
@@ -1,7 +1,7 @@
 #include "newbignum.h"
 #include <string.h>
 #define DIGIT_SIZE 32
-#define WORD_SIZE 4294967296
+#define BASE 4294967296
 
 uint64_t
 bin_to_int(char *num)
@@ -45,7 +45,7 @@ bin_to_big(char *source)
 }
 
 uint64_t*
-add_big(uint64_t *a, uint64_t *b)
+sum_big(uint64_t *a, uint64_t *b)
 {
 	int t = 0;
 	while(a[t] || b[t]){
@@ -54,12 +54,75 @@ add_big(uint64_t *a, uint64_t *b)
 	uint64_t *c = (uint64_t*)malloc(sizeof(uint64_t) * DIGIT_SIZE);
 	int e = 0;
 	uint64_t w = a[0] + b[0];
-	c[0] = w % WORD_SIZE;
-	e = (int)(w / WORD_SIZE);
+	c[0] = w % BASE;
+	if(w > BASE){
+		e = 1;
+	}
 	for (int i = 1; i < t; ++i){
 		w = e + a[i] + b[i];
-		c[i] = w % WORD_SIZE;
-		e = (int)(w / WORD_SIZE);
+		c[i] = w % BASE;
+		if(w > BASE){
+			e = 1;
+		}
+		else e = 0;
+	}
+	for (int i = 0; i < 2 * t; ++i)
+	{
+		printf("%lu\n", c[i]);
+	}
+	return c;
+}
+
+uint64_t*
+sub_big(uint64_t *a, uint64_t *b)
+{
+	int t = 0;
+	while(a[t] || b[t]){
+		t++;
+	}
+	uint64_t *c = (uint64_t*)malloc(sizeof(uint64_t) * DIGIT_SIZE);
+	int e = 0;
+	uint64_t w = a[0] - b[0];
+	if(w < 0){
+		w = w + BASE;
+		e = 1;
+	}
+	c[0] = w % BASE;
+	for (int i = 1; i < t; ++i){
+		w = a[i] - b[i] - e;
+		if(w < 0){
+			w = w + BASE;
+			e = 1;
+		}
+		else e = 0;
+		c[i] = w % BASE;
+	}
+	return c;
+}
+
+
+
+uint64_t*
+mul_big(uint64_t *a, uint64_t *b)
+{
+	int t = 0;
+	while(a[t] || b[t]){
+		t++;
+	}
+	uint64_t *c = (uint64_t*)malloc(sizeof(uint64_t) * DIGIT_SIZE);
+	memset(c, 0, DIGIT_SIZE);
+	uint32_t hi = 0, lo = 0;
+	uint64_t res = 0;
+	for (int i = 0; i < t; ++i){
+		hi = 0;
+		for (int j = 0; j < t; ++j){
+			// uint64_t 0xFFFFFFFFFFFFFFFF
+			res = c[i + j] + (a[i] * b[j]) + hi;
+			hi = (res >> 32) & 0xFFFFFFFF;
+			lo = res & 0xFFFFFFFF;
+			c[i + j] = lo;
+		}
+		c[i + t] = hi;
 	}
 	return c;
 }
@@ -68,9 +131,12 @@ int
 main(int argc, char const *argv[])
 {
 	char *num_1 = "1010111111111111110110100011101011110111111111111000101001110101100010100111010011111100101011101101000011010111111110101011101010";
+	char *num_2 = "1111111111111111110110101011101011110111111111111000101001110101100010100111010011111100101011101101000011010111111110101011101011";
 	uint64_t *big_1 = bin_to_big(num_1);
-	uint64_t *big_2 = add_big(big_1, big_1);
+	uint64_t *big_3 = bin_to_big(num_2);
+	uint64_t *big_2 = mul_big(big_3, big_1);
 	free(big_1);
 	free(big_2);
+	free(big_3);
 	return 0;
 }
